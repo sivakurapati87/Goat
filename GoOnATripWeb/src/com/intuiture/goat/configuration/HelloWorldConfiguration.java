9317894e13c01547e916ddc7f9f799b214f08ada
siva <sivakurapati@gmail.com>
2016-01-12 15:17:21 -0800

    Intial and refactoring the files

--- GoOnATripWeb/src/com/intuiture/goat/configuration/HelloWorldConfiguration.java
+++ GoOnATripWeb/src/com/intuiture/goat/configuration/HelloWorldConfiguration.java
@@ -1,45 +1,45 @@
-package com.intuiture.goat.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
-import org.springframework.web.servlet.view.velocity.VelocityView;
-import org.springframework.web.servlet.view.velocity.VelocityViewResolver;
-
-@Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = "com.intuiture.goat")
-public class HelloWorldConfiguration extends WebMvcConfigurerAdapter{
-	
-	
-	@Bean
-	public VelocityConfigurer velocityConfig() {
-	    VelocityConfigurer velocityConfigurer = new VelocityConfigurer();
-	    velocityConfigurer.setResourceLoaderPath("/WEB-INF/velocity/");
-	    return velocityConfigurer;
-	}
-	
-	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
-		VelocityViewResolver viewResolver = new VelocityViewResolver();
-		
-		viewResolver.setViewClass(VelocityView.class);
-		viewResolver.setCache(true);
-		viewResolver.setPrefix("");
-		viewResolver.setSuffix(".jsp");
-		viewResolver.setExposeSpringMacroHelpers(true);
-
-		registry.viewResolver(viewResolver);
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}
-
-}
+//package com.intuiture.goat.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
+//import org.springframework.web.servlet.view.velocity.VelocityView;
+//import org.springframework.web.servlet.view.velocity.VelocityViewResolver;
+//
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan(basePackages = "com.intuiture.goat")
+//public class HelloWorldConfiguration extends WebMvcConfigurerAdapter{
+//	
+//	
+//	@Bean
+//	public VelocityConfigurer velocityConfig() {
+//	    VelocityConfigurer velocityConfigurer = new VelocityConfigurer();
+//	    velocityConfigurer.setResourceLoaderPath("/WEB-INF/velocity/");
+//	    return velocityConfigurer;
+//	}
+//	
+//	@Override
+//	public void configureViewResolvers(ViewResolverRegistry registry) {
+//		VelocityViewResolver viewResolver = new VelocityViewResolver();
+//		
+//		viewResolver.setViewClass(VelocityView.class);
+//		viewResolver.setCache(true);
+//		viewResolver.setPrefix("");
+//		viewResolver.setSuffix(".jsp");
+//		viewResolver.setExposeSpringMacroHelpers(true);
+//
+//		registry.viewResolver(viewResolver);
+//	}
+//
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//	}
+//
+//}
